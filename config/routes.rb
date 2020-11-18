c06bc55f718de9b94a8615c80df921bc01b1c8c0
EC2 Default User <ootakaebisu@gmail.com>
2020-11-18 04:49:05 +0000

    [add]お手本URLをroutes.rbにコメントで追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# お手本URL
+# http://freecamp.life/rails-follow-follower/
